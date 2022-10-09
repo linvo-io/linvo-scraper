c79d78ea98b25d6222c8e2e22ac54290d9232da4
Nevo David <nevo@notu.co>
2022-10-09 18:48:38 +0700

    fix: added res

--- lib/test.example.ts
+++ lib/test.example.ts
@@ -2,20 +2,28 @@ import * as LinvoScraper from './linkedin.service';
 import * as puppeteer from 'puppeteer';
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false
+    });
     const page = (await browser.newPage());
     const cdp = await page.target().createCDPSession();
 
-// add ghost-cursor for maximum safety
+    // that's the res Linvo is working in production
+    await page.setViewport({
+        width: 1440,
+        height: 900,
+    });
+
+    // add ghost-cursor for maximum safety
     await LinvoScraper.tools.loadCursor(page, true);
 
-// Login with Linkedin
+    // Login with Linkedin
     const {token} = await LinvoScraper.services.login.process(page, cdp, {
         user: 'bang@linvo.io',
         password: 'superStrongPass!!%'
     })
 
-// set cookies
+    // set cookies
     await page.setCookie({
         name: "li_at",
         value: token,
